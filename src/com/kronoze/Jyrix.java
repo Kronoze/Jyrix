bfb55a13c96b17905499684ac454a5999d0a546b
Ryan <ryan.mewhorter@gmail.com>
2014-11-19 15:04:55 -0600

    started listeners, improved organization

--- src/com/kronoze/Jyrix.java
+++ src/com/kronoze/Jyrix.java
@@ -9,33 +9,52 @@ import java.util.Properties;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Jyrix {
+public class Jyrix extends JFrame implements ActionListener, WindowListener, KeyListener {
 
+	private static final long serialVersionUID = 1L;
 	private static Properties settings = new Properties();
 	private static File directory = new File("C:\\Users\\dy9506qm.NW\\Desktop\\Jyrix");
 	
 	private static JTextField songInput;
+	private static JButton submit;
 	
-	@SuppressWarnings("unused")
-	private static void createWindow() {
-		JFrame frame = new JFrame("Jyrix");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(300, 200));
+	public Jyrix() {
+		this.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setPreferredSize(new Dimension(300, 200));
 		
-		songInput = new JTextField(1);
+		// song input field
+		songInput = new JTextField(10);
+		//songInput.setSize(new Dimension(70, 20));
+		songInput.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-		frame.setLocationRelativeTo(null);
-		frame.pack();
-		frame.setVisible(true);
+		// submit button
+		submit = new JButton("Submit");
+		
+		this.add(songInput);
+		this.add(submit);
+		this.setLocationRelativeTo(null);
+		this.pack();
+		this.setVisible(true);
+		this.addWindowListener(this);
+		this.addKeyListener(this);
+		try {
+			init();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public static String now() {
-		return (new java.util.Date()).toString();
-	}
-	
-	public static void main(String[] args) throws IOException {
+	private static void init() throws IOException {
 		directory.mkdirs();
 		File settingsFile = new File(directory, ".properties");
 		
@@ -55,8 +74,61 @@ public class Jyrix {
 		
 		
 		output.close();
+	}
+	
+	public static String now() {
+		return (new java.util.Date()).toString();
+	}
+	
+	public static long time() {
+		return (new java.util.Date()).getTime();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new Jyrix();
+	}
+
+	public void windowOpened(WindowEvent e) {
+		System.out.println("[" + time() + "] Window opened");
+	}
+
+	public void windowClosing(WindowEvent e) {
+		System.out.println("[" + time() + "] Window closing");
+	}
+
+	public void windowClosed(WindowEvent e) {
+		System.out.println("[" + time() + "] Window closed");
+	}
+
+	public void windowIconified(WindowEvent e) {
+		System.out.println("[" + time() + "] Window minimized");		
+	}
+
+	public void windowDeiconified(WindowEvent e) {
+		System.out.println("[" + time() + "] Window maximized");		
+	}
+
+	public void windowActivated(WindowEvent e) {
+		System.out.println("[" + time() + "] Window activated");		
+	}
+
+	public void windowDeactivated(WindowEvent e) {
+		System.out.println("[" + time() + "] Window deactivated");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("[" + time() + "] Action performed, " + e.toString());
+	}
+
+	public void keyTyped(KeyEvent e) {
 		
-		Jyrix.createWindow();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
 		
 	}
 	
